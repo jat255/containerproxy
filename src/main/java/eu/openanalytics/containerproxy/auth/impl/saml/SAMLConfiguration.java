e1fa6551decb3a10bd414740c1d20da7456bc835
Joshua Taillon <joshua.taillon@nist.gov>
2021-01-21 15:47:45 -0700

    add CustomSAMLBootstrap to enable SHA256 on SAML message

--- src/main/java/eu/openanalytics/containerproxy/auth/impl/saml/SAMLConfiguration.java
+++ src/main/java/eu/openanalytics/containerproxy/auth/impl/saml/SAMLConfiguration.java
@@ -117,7 +117,7 @@ public class SAMLConfiguration {
 
 	@Bean
 	public static SAMLBootstrap samlBootstrap() {
-		return new SAMLBootstrap();
+		return new CustomSAMLBootstrap();
 	}
 
 	@Bean
@@ -338,4 +338,14 @@ public class SAMLConfiguration {
 			super(chain);
 		}
 	}
+}
+
+public final class CustomSAMLBootstrap extends SAMLBootstrap {
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        super.postProcessBeanFactory(beanFactory);
+        BasicSecurityConfiguration config = (BasicSecurityConfiguration) Configuration.getGlobalSecurityConfiguration();
+        config.registerSignatureAlgorithmURI("RSA", SignatureConstants.ALGO_ID_SIGNATURE_RSA_SHA256);
+        config.setSignatureReferenceDigestMethod(SignatureConstants.ALGO_ID_DIGEST_SHA256);
+    }
 }
